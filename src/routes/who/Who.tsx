d4eeaed1cb18607b55ce52159ea33eabed0a676b
Ruby <6656623+rubychill@users.noreply.github.com>
2024-09-19 09:40:35 +1000

    rejigged who page

--- src/routes/who/Who.tsx
+++ src/routes/who/Who.tsx
@@ -4,7 +4,7 @@ import styles from './Who.module.scss';
 export const Who = () => {
     return <div class={classnames(styles.who)}>
         <h1>hi i'm ruby~</h1>
-        <p class={styles.gay}><span>t</span><span>r</span><span>a</span><span>n</span><span>s</span> <span>g</span><span>a</span><span>y</span> hacker nerd</p>
+        <p class={styles.gay}><span>g</span><span>a</span><span>y</span> <span>t</span><span>r</span><span>a</span><span>n</span><span>s</span> hacker nerd</p>
         <p><a href="https://github.com/rubychill">github</a></p>
     </div>
 }
